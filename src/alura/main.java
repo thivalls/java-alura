5f74d2cc1187dc4de66a8fff6dad7462f92fc7c3
valls <olavalls@gmail.com>
2021-02-02 22:29:28 -0300

    Initial commit

---
+++ src/alura/main.java
@@ -0,0 +1,14 @@
+package alura;
+
+public class main {
+
+	public static void main(String[] args) {
+		String name = name;
+		
+		function printLnName() {
+			System.out.println(name);
+		}
+
+	}
+
+}
